8bf667b629708a0ad10d9e3c1efcd867170aecba
No Face <qngnud@gmail.com>
2019-12-01 20:06:52 +0700

    update project

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,167 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div class="hello">
+        <h1>{{ msg }}</h1>
+
+        <div id="app">
+            <form action="" v-cloak id="main">
+                <div class="bar">
+                    <label>
+                        <input type="text" v-model="searchString" placeholder="Enter your search terms">
+                    </label>
+                </div>
+                <ul>
+                    <li v-for="article in filteredArticle" :key="article.id">
+                        <a :href="article.url">
+                            <img :src="article.image" alt="">
+                            <p>{{ article.title}}</p>
+                        </a>
+                    </li>
+                </ul>
+            </form>
+        </div>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+    export default {
+        name: 'HelloWorld',
+        props: {
+            msg: String
+        },
+        data: function () {
+            return {
+                searchString: '',
+                articles: [
+                    {
+                        title: "What You Need To Know About CSS Variables",
+                        url: "https://tutorialzine.com/2016/03/what-you-need-to-know-about-css-variables/",
+                        image: "https://tutorialzine.com/media/2016/03/css-variables.jpg"
+                    },
+                    {
+                        title: "Freebie: 4 Great Looking Pricing Tables",
+                        url: "https://tutorialzine.com/2016/02/freebie-4-great-looking-pricing-tables/",
+                        image: "https://tutorialzine.com/media/2016/02/great-looking-pricing-tables.jpg"
+                    },
+                    {
+                        title: "20 Interesting JavaScript and CSS Libraries for February 2016",
+                        url: "https://tutorialzine.com/2016/02/20-interesting-javascript-and-css-libraries-for-february-2016/",
+                        image: "https://tutorialzine.com/media/2016/02/interesting-resources-february.jpg"
+                    },
+                    {
+                        title: "Quick Tip: The Easiest Way To Make Responsive Headers",
+                        url: "https://tutorialzine.com/2016/02/quick-tip-easiest-way-to-make-responsive-headers/",
+                        image: "https://tutorialzine.com/media/2016/02/quick-tip-responsive-headers.png"
+                    },
+                    {
+                        title: "Learn SQL In 20 Minutes",
+                        url: "https://tutorialzine.com/2016/01/learn-sql-in-20-minutes/",
+                        image: "https://tutorialzine.com/media/2016/01/learn-sql-20-minutes.png"
+                    },
+                    {
+                        title: "Creating Your First Desktop App With HTML, JS and Electron",
+                        url: "https://tutorialzine.com/2015/12/creating-your-first-desktop-app-with-html-js-and-electron/",
+                        image: "https://tutorialzine.com/media/2015/12/creating-your-first-desktop-app-with-electron.png"
+                    }
+                ]
+            }
+        },
+        computed: {
+            filteredArticle: function () {
+                let article_array = this.articles,
+                    searchString = this.searchString;
+                if (!searchString) {
+                    return article_array
+                }
+                searchString = searchString.trim().toLocaleLowerCase();
+                article_array = article_array.filter(function (item) {
+                    if (item.title.toLowerCase().indexOf(searchString) !== -1) {
+                        return item
+                    }
+                });
+                return article_array
+            }
+        }
+    }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+    [v-cloak] {
+        display: none;
+    }
+
+    h3 {
+        margin: 40px 0 0;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }
+
+    .bar {
+        background-color: #42b983;
+
+        box-shadow: 0 1px 1px #ccc;
+        border-radius: 2px;
+        width: 400px;
+        padding: 14px;
+        margin: 45px auto 20px;
+        position: relative;
+    }
+
+    .bar input {
+        background: #fff no-repeat 13px 13px;
+
+        border: none;
+        width: 100%;
+        line-height: 19px;
+        padding: 11px 0;
+
+        border-radius: 2px;
+        box-shadow: 0 2px 8px #c4c4c4 inset;
+        text-align: left;
+        font-size: 14px;
+        font-family: inherit;
+        color: #738289;
+        font-weight: bold;
+        outline: none;
+        text-indent: 40px;
+    }
+
+    ul {
+        list-style: none;
+        width: 428px;
+        margin: 0 auto;
+        text-align: left;
+    }
+
+    ul li {
+        border-bottom: 1px solid #ddd;
+        padding: 10px;
+        overflow: hidden;
+    }
+
+    ul li img {
+        width: 60px;
+        height: 60px;
+        float: left;
+        border: none;
+    }
+
+    ul li p {
+        margin-left: 75px;
+        font-weight: bold;
+        padding-top: 12px;
+        color: #6e7a7f;
+    }
 </style>
